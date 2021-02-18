b9a37c08e017284a8b70eb2dea2ee8db9ac88f66
berzezek <wkduz@gmai.com>
2021-02-18 16:49:12 +0500

    verion 1.1.2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-console.log("Первое задание:\n\n")
-
+console.log("   Первое задание:\n\n");
+document.write("   Первое задание: <br><br>");
 let num_1 = 0;
 let num_2 = 0;
 for (let i = 0; i <= 1000; i++){
@@ -7,23 +7,28 @@ for (let i = 0; i <= 1000; i++){
     num_1 += i;
   }
 }
-console.log("Сумма чисел кратных 3 в диапазоен 1 - 1000 равна: " + num_1);
+console.log("Сумма чисел кратных 3 в диапазоне 1 - 1000 равна: " + num_1);
+document.write("Сумма чисел кратных 3 в диапазоне 1 - 1000 равна: " + num_1, "<br>");
 for (let i = 0; i <= 1000; i++){
   if (i % 5 == 0){
     num_2 += i;
   }
 }
-console.log("Сумма чисел кратных 5 в диапазоен 1 - 1000 равна: ","+" + num_2, "\n\n\n\n");
+console.log("Сумма чисел кратных 5 в диапазоне 1 - 1000 равна: ","+" + num_2, "\n\n\n\n");
+document.write("Сумма чисел кратных 5 в диапазоне 1 - 1000 равна: ","+" + num_2, "<br><br><br><br>");
 
-console.log("Второе задание:\n\n")
+console.log("   Второе задание:\n\n")
+document.write("   Второе задание:<br><br>")
 
 var x = new Array(new Array(20, 34, 2), new Array(9, 12, 18), new Array(3, 4, 5));
 let num = 1/0;
 x.forEach(function(item, i, array){
-  item.forEach((item, i, array) => {
+  item.forEach((item, i, array) => { //После долгих и мучительных проб 'атом' сам поставил => ;)
     if(item < num){
       num = item;
     }
   });
 });
-console.log("Минимальный элемент массива: " + num);
+
+console.log("Минимальный элемент массива: " + x + " равен " + num);
+document.write("Минимальный элемент массива: " + x + " равен " + num);
